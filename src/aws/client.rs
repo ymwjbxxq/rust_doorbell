b2c33386490a26c838ffaab144170a4e67db5033
Dan <78874812+ymwjbxxq@users.noreply.github.com>
2022-01-16 09:15:20 +0100

    fix(#1): Create CloudFormation structure for websocket (#6)
    * add gitignore
    
    * change s3 name
    
    * add websocket infra
    
    * add lambda template
    
    * remove comment for s3 infrastructure
    
    * remove single quote from yaml template
    
    Co-authored-by: Daniele <>

---
+++ src/aws/client.rs
@@ -0,0 +1,34 @@
+pub struct AWSConfig {
+  config: aws_types::config::Config
+}
+
+impl AWSConfig {
+  pub fn set_config(config: aws_types::config::Config) -> Self {
+    Self { 
+      config 
+    }
+  }
+
+  pub fn init(&self) -> AWSClient {
+    let aws_client = AWSClient {
+      dynamo_db_client: self.dynamo_client(),
+      s3_client: self.s3_client(),
+    };
+
+    aws_client
+  }
+
+  fn dynamo_client(&self) -> aws_sdk_dynamodb::Client {
+    aws_sdk_dynamodb::Client::new(&self.config)
+  }
+
+  fn s3_client(&self) -> aws_sdk_s3::Client {
+    aws_sdk_s3::Client::new(&self.config)
+  }
+}
+
+#[derive(Clone)]
+pub struct AWSClient {
+  pub dynamo_db_client: aws_sdk_dynamodb::Client,
+  pub s3_client: aws_sdk_s3::Client,
+}
---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+pub mod error;
+pub mod utils;
+pub mod dtos;
+pub mod aws;
