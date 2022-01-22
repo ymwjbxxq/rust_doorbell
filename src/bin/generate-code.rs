b69e52671da7a58183af42815f0c50ff86c46dfe
Dan <78874812+ymwjbxxq@users.noreply.github.com>
2022-01-22 18:58:42 +0100

    code generation (#22)
    Co-authored-by: Daniele <>

--- src/bin/generate-code.rs
+++ src/bin/generate-code.rs
@@ -1,29 +1,31 @@
-use rust_doorbell::aws::client::AWSClient;
-use aws_sdk_dynamodb::model::AttributeValue;
-use rust_doorbell::aws::client::{AWSConfig};
 use lambda_runtime::{handler_fn, Context, Error};
 use rust_doorbell::error::ApplicationError;
-use tracing::{info};
-use rust_doorbell::{utils::*};
-use serde_json::{json, Value};
+use tracing::info;
+use rust_doorbell::utils::*;
+use rand::Rng;
+use serde_json::Value;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
   setup_tracing();
-  // Initialize AWS client
-  let config = aws_config::load_from_env().await;
-  let config = AWSConfig::set_config(config);
-  let aws_client = config.on_disconnect();
 
   lambda_runtime::run(handler_fn(|event: Value, ctx: Context| {
-    execute(&aws_client, event, ctx)
+    execute(event, ctx)
   }))
   .await?;
   Ok(())
 }
 
-pub async fn execute(aws_client: &AWSClient, event: Value, _ctx: Context) -> Result<String, ApplicationError> {
+pub async fn execute(event: Value, _ctx: Context) -> Result<String, ApplicationError> {
   info!("EVENT {:?}", event);
+  Ok(format!(
+      "{part1}-{part2}",
+      part1 = generate_random(),
+      part2 = generate_random()
+    ))
+}
 
-  Ok(String::from("code"))
+fn generate_random() -> String {
+  let mut rng = rand::thread_rng();
+  rng.gen_range(0..999).to_string()
 }
