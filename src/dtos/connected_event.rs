73f02f3df888ef52133a0934152044f7a7d7ff66
Daniele <>
2022-01-23 09:31:54 +0100

    export WebSocketDomain in different lamba functions

--- src/dtos/connected_event.rs
+++ src/dtos/connected_event.rs
@@ -3,5 +3,4 @@ use serde::{Deserialize, Serialize};
 #[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
 pub struct ConnectedEvent {
   pub connection_id: String,
-  pub endpoint: String,
 }
