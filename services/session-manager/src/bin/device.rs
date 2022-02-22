61a8aa25e3e6fb65b4739a4d1769f9df0fd8004c
Daniele <>
2022-02-22 09:42:22 +0100

    replace match with if else

--- services/session-manager/src/bin/device.rs
+++ services/session-manager/src/bin/device.rs
@@ -1,11 +1,14 @@
-use std::time::Duration;
-use aws_config::{TimeoutConfig, RetryConfig};
+use aws_config::{RetryConfig, TimeoutConfig};
 use lambda_http::{http::StatusCode, service_fn, Error, IntoResponse, Request, RequestExt};
 use serde_json::json;
-use session_manager::{models::device_request::DeviceRequest, 
-  utils::{dynamodb::AttributeValuesExt, api_helper::ApiHelper}, 
-  commands::update_subscription_device::{UpdateSubscriptionDevice, UpdateSubscriptionDeviceCommanad}}
-;
+use session_manager::{
+    commands::update_subscription_device::{
+        UpdateSubscriptionDevice, UpdateSubscriptionDeviceCommanad,
+    },
+    models::device_request::DeviceRequest,
+    utils::{api_helper::ApiHelper, dynamodb::AttributeValuesExt},
+};
+use std::time::Duration;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -28,32 +31,35 @@ async fn main() -> Result<(), Error> {
     Ok(())
 }
 
-pub async fn execute(client: &aws_sdk_dynamodb::Client, event: Request) -> Result<impl IntoResponse, Error> {
+pub async fn execute(
+    client: &aws_sdk_dynamodb::Client,
+    event: Request,
+) -> Result<impl IntoResponse, Error> {
     let body = event.payload::<DeviceRequest>()?;
 
     if let Some(device_request) = body {
-        let result = UpdateSubscriptionDevice::new().await
+        let result = UpdateSubscriptionDevice::new()
+            .await
             .execute(&client, &device_request)
             .await;
 
-        Ok(match result {
-            Ok(result) => {
-                let attributes = result.attributes.unwrap();
-                ApiHelper::response(
-                    StatusCode::OK,
-                json!({ 
-                    "message": format!("Remaining devices: {:?}", attributes.get_string("devices")), 
-                    "remaining_devices": attributes.get_number("devices") 
-                }).to_string())
-            }
-            _ => {
-              println!("ERROR {:?}", result.err().unwrap());
-              ApiHelper::response(
-                  StatusCode::INTERNAL_SERVER_ERROR,
-                  json!({ "message": "Limit reached, move to PRO plan" }).to_string(),
-              )
-            },
-        })
+        if let Ok(result) = result {
+            let attributes = result.attributes.unwrap();
+            Ok(ApiHelper::response(
+                StatusCode::OK,
+                json!({
+                    "message": format!("Remaining devices: {:?}", attributes.get_string("devices")),
+                    "remaining_devices": attributes.get_number("devices")
+                })
+                .to_string(),
+            ))
+        } else {
+            println!("ERROR {:?}", result.err().unwrap());
+            Ok(ApiHelper::response(
+                StatusCode::INTERNAL_SERVER_ERROR,
+                json!({ "message": "Limit reached, move to PRO plan" }).to_string(),
+            ))
+        }
     } else {
         Ok(ApiHelper::response(
             StatusCode::BAD_REQUEST,
